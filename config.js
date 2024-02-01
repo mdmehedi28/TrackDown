581174e5aa6325046fbb0af61b3b9bf7d31a9773
mdmehedi28 <158441082+mdmehedi28@users.noreply.github.com>
2024-02-01 12:37:28 +0600

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // config.js
 module.exports = {
-   botToken: "YOUR_BOT_TOKEN", // Modify your bot TOKEN here
+   botToken: "6424606460:AAGVbOJOTFeiJeE8VYsZ9M3BYoJeoRP4MyQ", // Modify your bot TOKEN here
    hostURL: "YOUR_URL", // Modify your host URL here
 };
 // for port forewording use "ssh -R 80:localhost:5000 serveo.net"
